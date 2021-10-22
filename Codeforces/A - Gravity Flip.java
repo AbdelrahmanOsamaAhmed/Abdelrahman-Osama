3c9723bc51a12098c2cbb2b263e23618ac6a457d
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2021-10-22 05:45:21 +0200

    Create A - Gravity Flip.java

---
+++ Codeforces/A - Gravity Flip.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= scan.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i : arr){
+            System.out.print(i+" ");
+ 
+        }    }
+}
