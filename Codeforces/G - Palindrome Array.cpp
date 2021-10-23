dc77f521786d74911720633352374117571d5b5c
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2021-10-23 23:39:23 +0200

    Create G - Palindrome Array.cpp

---
+++ Codeforces/G - Palindrome Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+    bool xl = true;
+    for(int i = 0 ; i < n ; i++){
+        if(arr[i] != arr[n-i-1]){
+            xl = false;
+            break;
+        }
+    }
+    if(xl)cout << "YES";
+    else cout << "NO";
+ 
+ 
+}
