c444f2eea4f181e7346846d91f59705e90708f65
Abdelrahman Osama <abdo.messi54@gmail.com>
2021-11-13 08:37:17 +0200

    spaces

--- Bank Management System/src/Main.java
+++ Bank Management System/src/Main.java
@@ -7,7 +7,5 @@ public class Main {
         AbdelrahmanOsama.Withdraw(2000,132595455,123);
         AbdelrahmanOsama.Withdraw(5000,13259547,123);
         AbdelrahmanOsama.Deposit(5000,13259547,123);
-
-
     }
 }
