94d59d71a84863ca04f4cea1a6712ebe57855417
Abdelrahman Osama <abdo.messi54@gmail.com>
2021-11-16 03:08:40 +0200

    Interacting main

--- Bank Management System/src/BankAccount.java
+++ Bank Management System/src/BankAccount.java
@@ -1,14 +1,20 @@
 public class BankAccount {
     private SecurityCodeChecker securityCode;
     private PinCodeChecker pinCode;
+    private String name;
     private Operation operate;
     private int balance;
 
-    public BankAccount (int securityCode, int pinCode){
+    public BankAccount (String name , int securityCode, int pinCode){
+        this.name=name;
         this.securityCode = new SecurityCodeChecker(securityCode);
         this.pinCode = new PinCodeChecker(pinCode);
         this.balance = 0;
         operate = new Operation(balance);
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public void Deposit(int amount, int securityCode, int pinCode){
@@ -16,10 +22,10 @@ public class BankAccount {
             System.out.println("Depositing....");
             operate.makeDeposit(amount);
             this.balance = operate.getBalance();
-            System.out.println("Operation succeeded!\n\n");
+            System.out.println("Operation succeeded!\n");
         }
         else {
-            System.out.println("Access denied!\n\n");
+            System.out.println("Access denied!\n");
         }
     }
 
@@ -27,16 +33,15 @@ public class BankAccount {
         if (this.securityCode.checkSecurityCode(securityCode) && this.pinCode.checkPinCode(pinCode)) {
             System.out.println("Checking balance....");
             if (operate.makeWithdraw(amount)) {
-                System.out.println("Operation succeeded!\n\n");
+                System.out.println("Operation succeeded!\n");
             } else {
-                System.out.println("Operation failed!\n\n");
+                System.out.println("Operation failed!\n");
             }
         }
         else {
-            System.out.println("Access denied!\n\n");
+            System.out.println("Access denied!\n");
         }
     }
-
 
 
 }
--- Bank Management System/src/Main.java
+++ Bank Management System/src/Main.java
@@ -1,11 +1,66 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount AbdelrahmanOsama = new BankAccount(13259547,123);
-        AbdelrahmanOsama.Deposit(5000,13259547,123);
-        AbdelrahmanOsama.Withdraw(2000,13259547,123);
-        AbdelrahmanOsama.Withdraw(2000,13259547,122);
-        AbdelrahmanOsama.Withdraw(2000,132595455,123);
-        AbdelrahmanOsama.Withdraw(5000,13259547,123);
-        AbdelrahmanOsama.Deposit(5000,13259547,123);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("""
+
+
+                Welcome to Virtual Bank. We ara happy to serve you.""");
+        ArrayList<BankAccount> accounts = new ArrayList<>();
+        while (true) {
+            System.out.println("""
+                    
+                    Kindly choose one of the following options:
+                    1- Open a new account.
+                    2- Deposit to an existing account.
+                    3- Withdraw from an existing account.
+                    4- Exit.""");
+
+
+            int option = scan.nextInt();
+            scan.nextLine();
+            if (option == 1) {
+                System.out.println("Please enter the name of your account: ");
+                String name = scan.nextLine();
+                System.out.println("Please enter a security code: ");
+                int securityCode = scan.nextInt();
+                System.out.println("Please enter a PIN code: ");
+                int pinCode = scan.nextInt();
+                accounts.add(new BankAccount(name, securityCode, pinCode));
+            } else if (option == 2) {
+                System.out.println("Please choose the account: ");
+                for (int i = 0 ; i < accounts.size() ; i++){
+                    System.out.println((i+1) + "- "+accounts.get(i).getName());
+                }
+                int account = scan.nextInt() - 1;
+                System.out.println("Please enter a security code: ");
+                int securityCode = scan.nextInt();
+                System.out.println("Please enter a PIN code: ");
+                int pinCode = scan.nextInt();
+                System.out.println("Please enter the amount of cash: ");
+                int amountOfCash = scan.nextInt();
+                accounts.get(account).Deposit(amountOfCash, securityCode, pinCode);
+            } else if (option == 3) {
+                System.out.println("Please choose the account: ");
+                for (int i = 0; i < accounts.size(); i++) {
+                    System.out.println((i + 1) + "- " + accounts.get(i).getName());
+                }
+                int account = scan.nextInt() - 1;
+                System.out.println("Please enter a security code: ");
+                int securityCode = scan.nextInt();
+                System.out.println("Please enter a PIN code: ");
+                int pinCode = scan.nextInt();
+                System.out.println("Please enter the amount of cash: ");
+                int amountOfCash = scan.nextInt();
+                accounts.get(account).Withdraw(amountOfCash, securityCode, pinCode);
+            }
+            else {
+                break;
+            }
+
+        }
+
     }
 }
