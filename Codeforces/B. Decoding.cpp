8311fe002655b69eb9217aab0fb67457f641255b
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2021-12-06 17:06:00 +0200

    Update B. Decoding.cpp

--- Codeforces/B. Decoding.cpp
+++ Codeforces/B. Decoding.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int size,median; cin >> size;
+    int size,median,l,r; cin >> size;
     string s; cin >> s;
     int n = size;
     char c[size];
@@ -10,24 +10,15 @@ int main(){
         c[n/2]=s[0];
         n--;
         s.erase(s.begin()+0);
-        int l = (n/2)-1;
-        int r = (n/2)+1;
-        while (s.size()>0){
-            c[l]=s[0];
-            s.erase(s.begin()+0);
-            c[r]=s[0];
-            s.erase(s.begin()+0);
-            l--;
-            r++;
-        }
-        for (int i = 0 ; i < size; i++){
-            cout << c[i];
-        }
+        l = (n/2)-1;
+        r = (n/2)+1;
+        
     }
     else {
-        int l = (n/2)-1;
-        int r = (n/2);
-        while (s.size()>0){
+         l = (n/2)-1;
+         r = (n/2);
+    }
+    while (s.size()>0){
             c[l]=s[0];
             s.erase(s.begin()+0);
             c[r]=s[0];
@@ -37,7 +28,5 @@ int main(){
         }
         for (int i = 0 ; i < size; i++){
             cout << c[i];
-        }
-    }
-    
+        }   
 }
